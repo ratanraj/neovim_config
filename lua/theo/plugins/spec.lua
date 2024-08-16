81acc36c76418bb9e375dce69d4f695f8fd70df0
Theophilus Ratanraj Ravuri <ratanraj.r@gmail.com>
2024-08-15 21:19:24 -0400

    treesitter

--- lua/theo/plugins/spec.lua
+++ lua/theo/plugins/spec.lua
@@ -1,3 +1,8 @@
 return {
-	{'nvim-treesitter/nvim-treesitter', build=':TSUpdate'}	
+	{'nvim-treesitter/nvim-treesitter', build=':TSUpdate'},
+	{'folke/todo-comments.nvim', opts = {} },
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' }
+	},
 }
